71b85e06d3f30afd7f2ef32a7f84b351a6d8955f
miltenkot <miltenkot@gmail.com>
2016-12-14 16:44:14 +0100

    Szablon startowy

---
+++ new_redy4s/DataViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DataViewController.swift
+//  new_redy4s
+//
+//  Created by Bartek Lanczyk on 12.12.2016.
+//  Copyright © 2016 miltenkot. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if self.revealViewController() != nil{
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+            revealViewController().rearViewRevealOverdraw = 0
+
+
+        // Do any additional setup after loading the view.
+    }
+}
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
